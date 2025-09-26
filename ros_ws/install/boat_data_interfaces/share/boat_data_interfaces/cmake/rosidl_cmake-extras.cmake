9d57c6a824690e8099d19ba54e3e740ef670e457
EmeraldWither <68785503+EmeraldWither@users.noreply.github.com>
2025-09-26 12:09:10 -0400

    remove install/build/logs directories

--- ros_ws/install/boat_data_interfaces/share/boat_data_interfaces/cmake/rosidl_cmake-extras.cmake
+++
@@ -1,4 +0,0 @@
-# generated from rosidl_cmake/cmake/rosidl_cmake-extras.cmake.in
-
-set(boat_data_interfaces_IDL_FILES "msg/ElectricalData.idl;msg/GPIOData.idl;msg/MotionData.idl;msg/MotorData.idl;msg/BoatAlarm.idl;msg/CANMotorData.idl")
-set(boat_data_interfaces_INTERFACE_FILES "msg/ElectricalData.msg;msg/GPIOData.msg;msg/MotionData.msg;msg/MotorData.msg;msg/BoatAlarm.msg;msg/CANMotorData.msg")
